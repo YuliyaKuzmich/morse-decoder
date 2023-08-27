8d011dbd5c3d790b27c57a8e52a845514021888c
Yuliya <kuzmich.yuliya@gmail.com>
2023-08-27 21:53:37 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,44 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    let result = '' ;
+    
+    let midResult = expr.match(/.{1,10}/g);
 
-module.exports = {
-    decode
+    
+
+
+    midResult.forEach(element => {
+        if (element === '**********') {
+            result += ' ';
+        } else {
+            const mid2 = element.match(/.{1,2}/g);
+            let newStr = '';
+            mid2.forEach(elem => {
+                if (elem === '10') {
+                    newStr += '.';
+                } else if (elem === '11') {
+                    newStr += '-';
+                } 
+            } )
+            result += MORSE_TABLE[newStr]
+        }
+        //result += MORSE_TABLE[newStr];
+    });
+
+
+    
+
+    return result;
+
+    //const mid2 = midResult.match(/.{1,2}/g);
+
+    
+    //console.log(midResult[3]);
+
 }
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
+
+/*module.exports = {
+    decode
+}*/
